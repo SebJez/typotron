7a845882e85d031a5d5fbd68278ed2d70fdd93cf
SebJez <sjezierski123@gmail.com>
2020-05-29 04:02:19 +0200

    add decoding key to ASCII (literal)
    literal - characters as on the typewriter keyboard.
    If a char is on KBII, pressing CODE+key on KBI will access it

--- typotron.ino
+++ typotron.ino
@@ -1,4 +1,4 @@
-#define ENCODING_UNIT_TEST
+//#define ENCODING_UNIT_TEST //uncomment to compile unit test instead of the main program
 
 
 #ifdef ENCODING_UNIT_TEST
@@ -9,6 +9,7 @@
 
 #include "io.h"
 #include "coder.h"
+#include "key_lut_literal.h"
 using namespace typotron;
 
 volatile byte inputMatrix[8];
@@ -21,15 +22,25 @@ void setup() {
   
 }
 
+  bool any_key;
+  byte prev_key;
+  bool prev_any_key = false;
+
 void loop() 
 {
   readToMatrix(inputMatrix);
 
-  bool any_key;
+
+
   byte key = matrixToKey(inputMatrix,&any_key);
 
-  if(any_key)Serial.println(String(key,8));
-  delay(300);
+  if(any_key) 
+  {
+    if(!prev_any_key || prev_key != key) Serial.print(keyToAsciiLiteral(key));
+  }
+
+  prev_key = key;
+  prev_any_key = any_key;
 
 
 }
